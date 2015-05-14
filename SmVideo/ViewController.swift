65b57722200fb923b73a2977459d4d23ec737f3d
cafish <jiajialuo@Jiajias-MacBook-Pro.local>
2015-05-13 20:01:57 -0700

    Initial Commit

---
+++ SmVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmVideo
+//
+//  Created by Jiajia Luo on 5/13/15.
+//  Copyright (c) 2015 PF3D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
